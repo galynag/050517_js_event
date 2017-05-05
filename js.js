2ecc8417129f82924acc2cd0a820c5369e6f3817
galynag <g.alenicheva@gmail.com>
2017-05-05 18:11:52 +0300

    3.нужно создать квадрат, который: 3.1. при нажатии на кнопку Esc скрыть данный блок; 3.2. при нажатии на кнопку m. блок должен отобразиться; 3.3. усложнение: (блок должен быть зафиксирован, и должен заезжать или выезжать)при наведении на блок должен выехать, при уходе курсора мышки с блока нужно скрывать блок, как при нажатии на кнопку Esc.

--- js.js
+++ js.js
@@ -35,6 +35,7 @@ var arrows = document.querySelector('.arrows');
 var x=0, y=0;
 
 function f3(event) {
+    console.log(event);
     event.preventDefault();
     console.log(x,y);
         if (event.key == 'ArrowRight'&& x<389 ) {
@@ -54,5 +55,32 @@ function f3(event) {
         console.log(x,y);
         arrows.style.top = y + 'px';
         }
-
  }
+
+ //нужно создать квадрат, который:
+//3.1. при нажатии на кнопку Esc скрыть данный блок;
+//3.2. при нажатии на кнопку m. блок должен отобразиться;
+//3.3. усложнение: (блок должен быть зафиксирован, и должен заезжать или выезжать)при наведении на блок должен выехать, при уходе курсора мышки с блока нужно скрывать блок, как при нажатии на кнопку Esc.
+
+document.addEventListener('keydown',f4);
+document.addEventListener('mouseover',f4);
+
+function f4(event) {
+    console.log(event);
+    event.preventDefault();
+    if (event.key == 'Escape') {
+        document.querySelector('.keyp2').style.left='-350px';
+    // document.querySelector('.keyp2').style.display='none'; это относится к заданию 3.2.
+    } else if (event.code == "KeyM") {
+        document.querySelector('.keyp2').style.left = '50px';
+        // document.querySelector('.keyp2').style.display='block'; это относится к заданию 3.2.
+    } else if (event.target.className == 'keyp2') {
+      document.querySelector('.keyp2').style.left='50px';
+    } else if (event.target.className != 'keyp2') {
+        document.querySelector('.keyp2').style.left='-350px';
+    }
+
+}
+
+
+
